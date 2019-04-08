a2e95a759a853c6f28be016676cefd44569e65be
Cedric Kalista <ckalista@gumtree.com>
2019-04-08 16:25:33 +0100

    Change zhs theme

--- runMe.sh
+++ runMe.sh
@@ -66,6 +66,16 @@ press_any_key_to_continue
 ansible-playbook mac_setup.yml -i hosts --ask-become-pass
 press_any_key_to_continue
 
+pretty_print "Installing Powerline fonts"
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 pretty_print "Installing ohmyzhc"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 pretty_print "Copying custom zhc config"
